e824796d9f4f3aa5357df2f585edaa321f0373e4
Mozzy Stix <cad8682@g.rit.edu>
2025-05-08 23:57:48 -0400

    I think I finished fixing D

---
+++ server/middleware/index.js
@@ -0,0 +1,36 @@
+//Not commenting out even though it's DomoMakerC because as long as I don't actually connect it, it won't affect anything
+const requiresLogin = (req, res, next) => {
+    if(!req.session.account) {
+        return res.redirect('/');
+    }
+    return next();
+}
+
+const requiresLogout = (req, res, next) => {
+    if(req.session.account) {
+        return res.redirect('/maker');
+    }
+
+    return next();
+}
+
+const requiresSecure = (req, res, next) => {
+    if(req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(`https://${req.hostname}${req.url}`);
+    }
+    return next();
+}
+
+const bypassSecure = (req, res, next) => {
+    next();
+}
+
+module.exports.requiresLogin = requiresLogin;
+module.exports.requiresLogout = requiresLogout;
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.requiresSecure = requiresSecure;
+}
+else {
+    module.exports.requiresSecure = bypassSecure;
+}
